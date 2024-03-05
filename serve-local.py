e8d8c66078b25f55086c7552eb162a53ba315399
Edmond Chui <1967998+EdmondChuiHW@users.noreply.github.com>
2024-03-05 16:13:43 +0000

    license

--- serve-local.py
+++ serve-local.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # Copyright (c) Meta Platforms, Inc. and affiliates.
+# Copyright 2019 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
