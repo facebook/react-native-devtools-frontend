5bbf2a261a31a860e96ea91b6ed7ab1e92f6bf67
Edmond Chui <1967998+EdmondChuiHW@users.noreply.github.com>
2024-03-11 12:28:57 +0000

    add stub

---
+++ front_end/embedder-static/embedderScript.js
@@ -0,0 +1,8 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Default no-op embedderScript stub. Impl can be served via embedders like Metro
+ */
