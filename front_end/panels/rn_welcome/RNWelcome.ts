1ebe8d092d6e8121e88cbc640ba93de86806efcc
Edmond Chui <1967998+EdmondChuiHW@users.noreply.github.com>
2024-03-25 15:40:31 +0000

    dynamic doc link

--- front_end/panels/rn_welcome/RNWelcome.ts
+++ front_end/panels/rn_welcome/RNWelcome.ts
@@ -3,11 +3,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import * as UI from '../../ui/legacy/legacy.js';
 import * as i18n from '../../core/i18n/i18n.js';
+import * as Root from '../../core/root/root.js';
+import * as UI from '../../ui/legacy/legacy.js';
+import * as LitHtml from '../../ui/lit-html/lit-html.js';
 
 import rnWelcomeStyles from './rnWelcome.css.js';
-import * as LitHtml from '../../ui/lit-html/lit-html.js';
 
 const UIStrings = {
   /** @description The name of the debugging product */
@@ -49,11 +50,9 @@ export class RNWelcomeImpl extends UI.Widget.VBox {
   }
 
   render(): void {
-    const welcomeIconUrl = new URL(
-      "../../Images/react_native/welcomeIcon.png",
-      import.meta.url,
-    ).toString();
-    render(html`
+    const welcomeIconUrl = new URL('../../Images/react_native/welcomeIcon.png', import.meta.url).toString();
+    render(
+        html`
       <div class="rn-welcome-panel">
         <div class="rn-welcome-header">
           <img class="rn-welcome-icon" src=${welcomeIconUrl} role="presentation" />
@@ -68,7 +67,7 @@ export class RNWelcomeImpl extends UI.Widget.VBox {
           ${i18nString(UIStrings.welcomeMessage)}
         </div>
         <div class="rn-welcome-links">
-          <x-link class="devtools-link" href="https://reactnative.dev/docs/debugging">
+          <x-link class="devtools-link" href=${this.getDocsLink()}>
             ${i18nString(UIStrings.docsLabel)}
           </x-link>
           <x-link class="devtools-link" href="https://reactnative.dev/blog">
@@ -76,6 +75,16 @@ export class RNWelcomeImpl extends UI.Widget.VBox {
           </x-link>
         </div>
       </div>
-    `, this.contentElement, {host: this});
+    `,
+        this.contentElement, {host: this});
+  }
+
+  private getDocsLink(): string {
+    const experimentDocsLink = Root.Runtime.experiments.enabledExperiments()
+                                   .find(e => e.name === Root.Runtime.ExperimentName.REACT_NATIVE_SPECIFIC_UI)
+                                   ?.docLink;
+    const defaultDocsLink = 'https://reactnative.dev/docs/debugging';
+
+    return experimentDocsLink ?? defaultDocsLink;
   }
 }
