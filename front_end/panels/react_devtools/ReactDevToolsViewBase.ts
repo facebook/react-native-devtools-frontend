59951e2c09fac4f71fc5749fced27dfe573baeba
Ruslan Lesiutin <hoxy@meta.com>
2025-06-10 19:18:15 +0100

    test

--- front_end/panels/react_devtools/ReactDevToolsViewBase.ts
+++ front_end/panels/react_devtools/ReactDevToolsViewBase.ts
@@ -3,9 +3,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import * as Common from '../../core/common/common.js';
-import * as Host from '../../core/host/host.js';
-import * as i18n from '../../core/i18n/i18n.js';
 import type * as Platform from '../../core/platform/platform.js';
 import * as SDK from '../../core/sdk/sdk.js';
 import * as Bindings from '../../models/bindings/bindings.js';
@@ -14,6 +11,9 @@ import * as Workspace from '../../models/workspace/workspace.js';
 import type * as ReactDevToolsTypes from '../../third_party/react-devtools/react-devtools.js';
 import * as ReactDevTools from '../../third_party/react-devtools/react-devtools.js';
 import * as UI from '../../ui/legacy/legacy.js';
+import * as Common from '../../core/common/common.js';
+import * as Host from '../../core/host/host.js';
+import * as i18n from '../../core/i18n/i18n.js';
 
 import {
   Events as ReactDevToolsModelEvents,
