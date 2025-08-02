49f518ddb846d8572dfd7a34ac9c0378a6910429
Devan Buggay <devanb@meta.com>
2025-08-02 12:10:01 -0700

    shelve changes to: Temporary Commit at 8/1/2025, 11:24:13 PM

--- front_end/panels/timeline/TimelineFlameChartDataProvider.ts
+++ front_end/panels/timeline/TimelineFlameChartDataProvider.ts
@@ -615,9 +615,7 @@ export class TimelineFlameChartDataProvider extends Common.ObjectWrapper.ObjectW
     // In CPU Profiles the trace data will not have frames nor
     // screenshots, so we can keep this call as it will be a no-op in
     // these cases.
-    if (!this.isReactNative) {
-      this.#appendFramesAndScreenshotsTrack();
-    }
+    this.#appendFramesAndScreenshotsTrack();
 
     const weight = (track: {type?: string, forMainFrame?: boolean, appenderName?: TrackAppenderName}): number => {
       switch (track.appenderName) {
